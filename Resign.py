48e934a7ae73b03de24e47ea11b8542d66c9f7de
lixinxing <lixx@eegsmart.com>
2016-04-09 16:48:51 +0800

    添加重签名脚本

---
+++ Resign.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+__author__ = 'lixinxing'
+# 用于重新签名
+
+import subprocess
+import os
+import Config
+import datetime
+
+
+
+class Resign:
+    def resipnIpa(self, ipaPath):
+        zipName = 'resign.zip'
+        # 获得当前py文件所在路径
+        # dir = os.path.join(os.path.dirname(__file__), 'history')
+        # print dir
+        # 获取各路径
+		# ipaPath = os.path.abspath(os.path.join(dir, 'PEPatient.ipa'))
+        print ipaPath
+
+        # 复制成.zip后缀
+        cpCmd = 'cp ' + ipaPath + ' ' + zipName
+        print cpCmd
+        process = subprocess.Popen(cpCmd, shell=True)
+        process.wait()
+        # 解压zip到指定目录
+        unzipCmd = 'unzip ' + zipName
+        print unzipCmd
+        process = subprocess.Popen(unzipCmd, shell=True)
+        process.wait()
+
+        #删除解压的文件
+        delZipCmd = 'rm -rf ' + zipName
+        process = subprocess.Popen(delZipCmd, shell=True)
+        process.wait()
+        appName = Config.scheme_name + '.app'
+        print appName
+        # 删除需要替换的文件
+        codisignPath = 'Payload/' + appName + '/_CodeSignature'
+        targetPath = 'Payload/'+ appName + '/embedded.mobileprovision'
+        delDirCmd = 'rm -rf ' + codisignPath + ' ' + targetPath
+        print delDirCmd
+        process = subprocess.Popen(delDirCmd, shell=True)
+        process.wait()
+        #
+        # 企业Profile的绝对路径
+        profilePath = 'template/' + Config.ep_provisioning_profile
+        # 需要替换的Profile的地址
+        targetPath = 'Payload/' + 'embedded.mobileprovision'
+        replaceCmd = 'cp ' + profilePath + ' ' + targetPath
+        print replaceCmd
+        process = subprocess.Popen(replaceCmd, shell=True)
+        process.wait()
+
+        # 重新签名 codesign -f -s $certifierName  --entitlements entitlements.plist Payload/test.app
+        eplistPath = os.path.join(os.path.dirname(__file__), 'template', Config.resign_plist_name)
+        signPath = 'Payload/' + appName
+        signCmd = 'codesign -f -s "' + Config.ep_cer_name + '" --entitlements ' + eplistPath + ' ' + signPath
+        print signCmd
+        process = subprocess.Popen(signCmd, shell=True)
+        process.wait()
+
+        # 重新打包
+        formt_time = datetime.datetime.now().strftime("%Y%m%d%H%M")
+        resignIpaName = 'Resign' + Config.scheme_name + formt_time + '.ipa'
+        reZipResultPath = os.path.join(os.path.dirname(__file__), 'history', resignIpaName)
+        rezipCmd = 'zip -r ' + reZipResultPath + ' ' + 'Payload'
+        print rezipCmd
+        process = subprocess.Popen(rezipCmd, shell=True)
+        process.wait()
+
+        # 删除Payload文件
+        delPayloadCmd = 'rm -fr ' + 'Payload'
+        process = subprocess.Popen(delPayloadCmd, shell=True)
+        process.wait()
+        return reZipResultPath
+
+
+
+if  __name__ == '__main__':
+    dir = os.path.join(os.path.dirname(__file__), 'history')
+    print dir
+    # 获取各路径
+    ipaPath = os.path.abspath(os.path.join(dir, 'test.ipa'))
+    new_ipa = Resign().resipnIpa(ipaPath)
+    print new_ipa
